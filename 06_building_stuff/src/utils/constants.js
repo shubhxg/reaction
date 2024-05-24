3b7b494926138411229f33840d980a81dd375acf
Shubh Sharma <mail.shubhsharma19@gmail.com>
2024-05-24 19:44:24 +0530

    fix cors issue using proxy server url

--- 06_building_stuff/src/utils/constants.js
+++ 06_building_stuff/src/utils/constants.js
@@ -5,4 +5,4 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const restaurantDataAPIURL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.3670355&lng=79.4304381&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
